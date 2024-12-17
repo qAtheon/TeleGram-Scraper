e58d4d2982f6a2bb9cda17ad0b61c77bbc1d84e6
Atheon <atheon.xq@gmail.com>
2024-12-17 17:04:24 +0300

    scraper.py

--- scraper.py
+++ scraper.py
@@ -12,12 +12,9 @@ cy="\033[1;36m"
 
 def banner():
     print(f"""
-{re}╔╦╗{cy}┌─┐┬  ┌─┐{re}╔═╗  ╔═╗{cy}┌─┐┬─┐┌─┐┌─┐┌─┐┬─┐
-{re} ║ {cy}├┤ │  ├┤ {re}║ ╦  ╚═╗{cy}│  ├┬┘├─┤├─┘├┤ ├┬┘
-{re} ╩ {cy}└─┘┴─┘└─┘{re}╚═╝  ╚═╝{cy}└─┘┴└─┴ ┴┴  └─┘┴└─
-
-            version : 3.1
-youtube.com/channel/UCnknCgg_3pVXS27ThLpw3xQ
+{cy}TELEGRAM ÜYE ÇEKİCİ
+          
+    {cy}Sürüm: 1.0
         """)
 
 cpass = configparser.RawConfigParser()
@@ -31,7 +28,7 @@ try:
 except KeyError:
     os.system('clear')
     banner()
-    print(re+"[!] run python3 setup.py first !!\n")
+    print(re+"[!] İlk önce python3 setup.py'yi çalıştırın !!\n")
     sys.exit(1)
 
 client.connect()
@@ -39,7 +36,7 @@ if not client.is_user_authorized():
     client.send_code_request(phone)
     os.system('clear')
     banner()
-    client.sign_in(phone, input(gr+'[+] Enter the code: '+re))
+    client.sign_in(phone, input(gr+'[+] Kodu girin: '+re))
  
 os.system('clear')
 banner()
@@ -59,44 +56,45 @@ chats.extend(result.chats)
  
 for chat in chats:
     try:
-        if chat.megagroup== True:
+        if chat.megagroup == True:
             groups.append(chat)
     except:
         continue
  
-print(gr+'[+] Choose a group to scrape members :'+re)
+print(gr+'[+] Üyeleri almak için bir grup seçin :'+re)
 i=0
 for g in groups:
     print(gr+'['+cy+str(i)+gr+']'+cy+' - '+ g.title)
     i+=1
  
 print('')
-g_index = input(gr+"[+] Enter a Number : "+re)
-target_group=groups[int(g_index)]
+g_index = input(gr+"[+] Bir numara girin: "+re)
+target_group = groups[int(g_index)]
  
-print(gr+'[+] Fetching Members...')
+print(gr+'[+] Üyeler alınıyor...')
 time.sleep(1)
 all_participants = []
 all_participants = client.get_participants(target_group, aggressive=True)
  
-print(gr+'[+] Saving In file...')
+print(gr+'[+] Dosyaya kaydediliyor...')
 time.sleep(1)
-with open("members.csv","w",encoding='UTF-8') as f:
-    writer = csv.writer(f,delimiter=",",lineterminator="\n")
-    writer.writerow(['username','user id', 'access hash','name','group', 'group id'])
+with open("members.csv", "w", encoding='UTF-8') as f:
+    writer = csv.writer(f, delimiter=",", lineterminator="\n")
+    writer.writerow(['kullanıcı adı', 'kullanıcı id', 'access hash', 'isim', 'grup', 'grup id'])
     for user in all_participants:
         if user.username:
-            username= user.username
+            username = user.username
         else:
-            username= ""
+            username = ""
         if user.first_name:
-            first_name= user.first_name
+            first_name = user.first_name
         else:
-            first_name= ""
+            first_name = ""
         if user.last_name:
-            last_name= user.last_name
+            last_name = user.last_name
         else:
-            last_name= ""
-        name= (first_name + ' ' + last_name).strip()
-        writer.writerow([username,user.id,user.access_hash,name,target_group.title, target_group.id])      
-print(gr+'[+] Members scraped successfully.')
+            last_name = ""
+        name = (first_name + ' ' + last_name).strip()
+        writer.writerow([username, user.id, user.access_hash, name, target_group.title, target_group.id])      
+
+print(gr+'[+] Üyeler başarıyla alındı.')
